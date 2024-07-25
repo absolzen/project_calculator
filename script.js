bd2a57df525d48b78cd844e73a56eded8074d95c
absolzen <robdomu@gmail.com>
2024-07-25 18:52:09 +0200

    Add execution function

--- script.js
+++ script.js
@@ -11,7 +11,29 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
-  // TODO: catch divide by zero case
+  // TODO: catch divide by zero case, maybe move the check to operate function
   return num1 / num2;
 }
 
+// declare operands and operator variables
+let operand1, operand2, operator;
+
+// execute function depending on the operator
+function operate(sign, n1, n2) {
+  switch (sign) {
+    case 'plus':
+      add(n1, n2);
+      break;
+    case 'minus':
+      subtract(n1, n2);
+      break;
+    case 'asterisk':
+      multiply(n1, n2);
+      break;
+    case 'slash':
+      divide(n1, n2);
+      break;
+    default:
+      return 'ERROR';
+  }
+}
