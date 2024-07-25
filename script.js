04e6e496b3b63f695c8367f45fc5ebdd4ef3660e
absolzen <robdomu@gmail.com>
2024-07-25 19:16:44 +0200

    Add reference to buttons

--- script.js
+++ script.js
@@ -37,3 +37,22 @@ function operate(sign, n1, n2) {
       return 'ERROR';
   }
 }
+
+const one = document.querySelector('.digit.one');
+const two = document.querySelector('.digit.two');
+const three = document.querySelector('.digit.three');
+const four = document.querySelector('.digit.four');
+const five = document.querySelector('.digit.five');
+const six = document.querySelector('.digit.six');
+const seven = document.querySelector('.digit.seven');
+const eight = document.querySelector('.digit.eight');
+const nine = document.querySelector('.digit.nine');
+const zero = document.querySelector('.digit.zero');
+
+const plus = document.querySelector('.sign.plus');
+const minus = document.querySelector('.sign.minus');
+const asterisk = document.querySelector('.sign.asterisk');
+const slash = document.querySelector('.sign.slash');
+
+const clear = document.querySelector('.other.clear');
+const equals = document.querySelector('.other.equals');
